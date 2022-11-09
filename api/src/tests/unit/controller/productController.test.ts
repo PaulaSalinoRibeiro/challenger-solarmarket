569c545879245cfa6f42593e8f471fc925153118
Paula <paulasalinor@gmail.com>
2022-11-09 07:05:06 -0300

    feat: add test in getAllProducts controller

---
+++ api/src/tests/unit/controller/productController.test.ts
@@ -0,0 +1,30 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { Request, Response, NextFunction } from 'express';
+import { ProductService } from '../../../services/productService';
+import { ProductController } from '../../../controllers/productController';
+import { productsMock } from '../../mock/products';
+
+const { expect } = chai;
+
+describe('Product Controller', () => {
+
+  const req = {} as Request;
+  const res = {} as Response;
+  const next: NextFunction = () => {};
+
+  const productService = new ProductService();
+  const productController = new ProductController(productService)
+
+  before(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  it('Should have return status 200 with a product list', () => {
+    productController.getAllProducts(req, res, next);
+
+    expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+    expect((res.json as sinon.SinonStub).calledWith(productsMock)).to.be.true;
+  });
+})
