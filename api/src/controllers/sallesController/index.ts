642878f1bdb3adb0f56ef28582040769443c67a9
Paula <paulasalinor@gmail.com>
2022-11-11 19:15:35 -0300

    feat: add createSalle and findByCep methods in SalleController class

--- api/src/controllers/sallesController/index.ts
+++ api/src/controllers/sallesController/index.ts
@@ -10,10 +10,27 @@ export class SalleController {
   getAllSalles(req: Request, res: Response, next: NextFunction) {
     try {
       const allSalles = this.service.getAllSalles();
-      res.status(200).json(allSalles)
+      res.status(200).json(allSalles);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
+  createSalle(req: Request, res: Response, next: NextFunction) {
+    try {
+      this.service.createSalle(req.body);
+      res.status(201).end();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  findByCep(req: Request, res: Response, next: NextFunction) {
+    try {
+      const salesByCep = this.service.findByCep(req.params.cep);
+      res.status(200).json(salesByCep);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
